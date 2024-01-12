d66dd6e2dd2310da733d680c13b6adfe316cfdf1
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-01-13 00:52:41 +0300

    feat: add donate link

--- src/app/components/wellcome/Wellcome.tsx
+++ src/app/components/wellcome/Wellcome.tsx
@@ -37,41 +37,45 @@ const Wellcome = () => {
 						</defs>
 					</svg>
 				</div>
-				<div>
-					<div className={styles.donation_wrapper}>
-						<p className={`${styles.donation} ${uikit.text5}`}>
-							поддержать проект
-						</p>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							width='60'
-							height='57'
-							viewBox='0 0 60 57'
-							fill='none'
+				<a href='https://t.me/rocknword/11681' target='_blank'>
+					<div>
+						<div className={styles.donation_wrapper}>
+							<p className={`${styles.donation} ${uikit.text5}`}>
+								поддержать проект
+							</p>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='60'
+								height='57'
+								viewBox='0 0 60 57'
+								fill='none'
+							>
+								<path
+									d='M49.2539 19.9512L57.7392 28.4365L49.2539 36.9217'
+									stroke='white'
+									stroke-width='2'
+								/>
+								<path
+									d='M57.7421 28.4374H29.3047H0.867309'
+									stroke='white'
+									stroke-width='2'
+								/>
+							</svg>
+						</div>
+
+						<div className={`${styles.slogan} ${uikit.text6}`}>
+							<p>Независимое радио без рекламы,</p>
+							<p>ипотеки и средств против импотенции —</p>
+						</div>
+
+						<div
+							className={`${styles.slogan_bold} ${uikit.text7} ${uikit.underline}`}
 						>
-							<path
-								d='M49.2539 19.9512L57.7392 28.4365L49.2539 36.9217'
-								stroke='white'
-								stroke-width='2'
-							/>
-							<path
-								d='M57.7421 28.4374H29.3047H0.867309'
-								stroke='white'
-								stroke-width='2'
-							/>
-						</svg>
+							<p>такое возможно?</p>
+							<p>Да, благодаря твоим донатам!</p>
+						</div>
 					</div>
-					<div className={`${styles.slogan} ${uikit.text6}`}>
-						<p>Независимое радио без рекламы,</p>
-						<p>ипотеки и средств против импотенции —</p>
-					</div>
-					<div
-						className={`${styles.slogan_bold} ${uikit.text7} ${uikit.underline}`}
-					>
-						<p>такое возможно?</p>
-						<p>Да, благодаря твоим донатам!</p>
-					</div>
-				</div>
+				</a>
 			</div>
 		</div>
 	)
