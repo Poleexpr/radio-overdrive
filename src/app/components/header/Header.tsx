31c097b7fae4b1359943e94352686d1fcde2dd39
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-01-16 22:32:41 +0300

    feat: add menuBurger

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, Events, animateScroll as scroll, scrollSpy } from 'react-scroll'
 
 import styles from './header.module.scss'
 import uikit from '../uikit.module.scss'
+import MenuBurger from '../menuBurger/MenuBurger'
 
 const Header = () => {
 	useEffect(() => {
@@ -25,48 +26,65 @@ const Header = () => {
 		console.log(to)
 	}
 
+	const [isOpen, setIsOpen] = useState(false)
+
+	const handleOnOpen = () => {
+		setIsOpen(!isOpen)
+		console.log(isOpen)
+	}
+
 	return (
-		<header className={styles.container}>
-			<nav className={styles.wrapper}>
-				<ul className={styles.menu}>
-					<li className={`${styles.menu_item} ${uikit.text8}`}>
-						<Link
-							activeClass='active'
-							to='about'
-							spy={true}
-							smooth={true}
-							offset={200}
-							duration={500}
-							onSetActive={handleSetActive}
-						>
-							о нас
-						</Link>
-					</li>
-					<li className={`${styles.menu_item} ${uikit.text8}`}>
-						<Link
-							activeClass='active'
-							to='presenters'
-							spy={true}
-							smooth={true}
-							offset={-10}
-							duration={500}
-							onSetActive={handleSetActive}
-						>
-							ведущие
-						</Link>
-					</li>
-					<li>
-						<img src='logo.svg'></img>
-					</li>
-					<li className={`${styles.menu_item} ${uikit.text8}`}>
-						<a href='#'>программа</a>
-					</li>
-					<li className={`${styles.menu_item} ${uikit.text8}`}>
-						<a href='#'>слушать</a>
-					</li>
-				</ul>
-			</nav>
-		</header>
+		<>
+			<header
+				id='outer-container'
+				className={isOpen ? styles.container : styles.isOpen}
+			>
+				<nav className={styles.wrapper}>
+					<ul className={styles.menu}>
+						<li className={`${styles.menu_item} ${uikit.text8}`}>
+							<Link
+								activeClass='active'
+								to='about'
+								spy={true}
+								smooth={true}
+								offset={200}
+								duration={500}
+								onSetActive={handleSetActive}
+							>
+								о нас
+							</Link>
+						</li>
+						<li className={`${styles.menu_item} ${uikit.text8}`}>
+							<Link
+								activeClass='active'
+								to='presenters'
+								spy={true}
+								smooth={true}
+								offset={-10}
+								duration={500}
+								onSetActive={handleSetActive}
+							>
+								ведущие
+							</Link>
+						</li>
+						<li>
+							<img src='logo.svg'></img>
+						</li>
+						<li className={`${styles.menu_item} ${uikit.text8}`}>
+							<a href='#'>программа</a>
+						</li>
+						<li className={`${styles.menu_item} ${uikit.text8}`}>
+							<a href='#'>слушать</a>
+						</li>
+					</ul>
+				</nav>
+				<MenuBurger
+					isOpen={isOpen}
+					onOpen={handleOnOpen}
+					onClose={handleOnOpen}
+				/>
+			</header>
+		</>
 	)
 }
 
--- src/app/components/menuBurger/MenuBurger.tsx
+++ src/app/components/menuBurger/MenuBurger.tsx
@@ -1,1 +1,81 @@
-import { slide as Menu } from 'react-burger-menu'
+import { elastic as Menu } from 'react-burger-menu'
+
+import styles from './menuBurger.module.scss'
+
+const MenuBurger = () => {
+	var nativeStyles = {
+		bmBurgerButton: {
+			position: 'fixed',
+			width: '36px',
+			height: '30px',
+			right: '36px',
+			top: '36px',
+		},
+		bmBurgerBars: {
+			background: '#373a47',
+		},
+		bmBurgerBarsHover: {
+			background: '#a90000',
+		},
+		bmCrossButton: {
+			height: '24px',
+			width: '24px',
+		},
+		bmCross: {
+			background: '#bdc3c7',
+		},
+		bmMenuWrap: {
+			position: 'fixed',
+			height: '100%',
+			top: '0',
+		},
+		bmMenu: {
+			background: '#373a47',
+			padding: '2.5em 1.5em 0',
+			fontSize: '1.15em',
+		},
+		bmMorphShape: {
+			fill: '#373a47',
+		},
+		bmItemList: {
+			color: '#b8b7ad',
+			padding: '0.8em',
+			display: 'flex',
+			flexDirection: 'column',
+		},
+		bmItem: {
+			display: 'inline-block',
+		},
+		bmOverlay: {
+			background: 'rgba(0, 0, 0, 0.3)',
+		},
+	}
+
+	return (
+		<Menu
+			right
+			isOpen={isOpen}
+			onOpen={handleOnOpen}
+			onClose={handleOnOpen}
+			customBurgerIcon={<img src='./burgerIcon.svg' />}
+			customCrossIcon={<img src='./crossIcon.svg' />}
+			styles={nativeStyles}
+			className={styles.menuBurger}
+		>
+			<a className='menu-item' href='/'>
+				Home
+			</a>
+			<a className='menu-item' href='/salads'>
+				Salads
+			</a>
+			<a className='menu-item' href='/pizzas'>
+				Pizzas
+			</a>
+			<a className='menu-item' href='/desserts'>
+				Desserts
+			</a>
+		</Menu>
+	)
+}
+
+export default MenuBurger
