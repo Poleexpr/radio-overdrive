6aeafc0cd508c1caf6d0850d10feefc7ccfcbf11
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-01-19 17:43:51 +0300

    feat: create aliens component

---
+++ src/app/components/aliens/Aliens.tsx
@@ -0,0 +1,10 @@
+import Marquee from 'react-fast-marquee'
+
+import styles from './aliens.module.scss'
+import uikit from '../uikit.module.scss'
+
+const Aliens = () => {
+	return <section></section>
+}
+
+export default Aliens
