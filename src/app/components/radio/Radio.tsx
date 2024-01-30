f38cada89ea6b6bcd7eda5859df40b647fddef2a
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-01-30 17:26:59 +0300

    feat: stylize the radio component

--- src/app/components/radio/Radio.tsx
+++ src/app/components/radio/Radio.tsx
@@ -1,11 +1,13 @@
 'use client'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
+import AudioPlayer from 'react-h5-audio-player'
+import { ReactSVG } from 'react-svg'
 
 import uikit from '../uikit.module.scss'
 import styles from './radio.module.scss'
 
 const Radio = () => {
-	const [stream, setStream] = useState('')
+	const [stream, setStream] = useState([])
 
 	useEffect(() => {
 		const getStream = async () => {
@@ -13,15 +15,32 @@ const Radio = () => {
 				'https://demo.azuracast.com/api/nowplaying_static/azuratest_radio.json'
 			)
 			const data = await rec.json()
-			console.log(data)
+			console.log(data.station.listen_url)
 			setStream(data)
 		}
 		getStream()
 	}, [])
 
 	return (
-		<section>
-			<div></div>
+		<section className={styles.container}>
+			<div className={styles.wrapper}>
+				<AudioPlayer
+					className='radio'
+					src='https://demo.azuracast.com/listen/azuratest_radio/radio.mp3'
+					onPlay={e => console.log('onPlay')}
+					layout='horizontal'
+					showJumpControls={false}
+					customVolumeControls={[]}
+					customAdditionalControls={[]}
+					showDownloadProgress={false}
+					customIcons={{
+						play: <ReactSVG src='play.svg' />,
+					}}
+				/>
+				<p className={`${uikit.text5} ${styles.live}`}>в эфире:</p>
+				<p className={`${uikit.text3} ${styles.title}`}>текст</p>
+				<p className={`${uikit.text} ${styles.artist}`}>текст</p>
+			</div>
 		</section>
 	)
 }
