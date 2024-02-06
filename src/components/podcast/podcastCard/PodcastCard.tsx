796fd19d59d7b289cf136561b306df0bf7a6b792
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-02-06 16:43:36 +0300

    fix: add FC type ty the PodcastCard

--- src/components/podcast/podcastCard/PodcastCard.tsx
+++ src/components/podcast/podcastCard/PodcastCard.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { useState } from 'react';
+import type { FC } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
-import typograf from 'typograf';
+import Typograf from 'typograf';
 
 import { Typography } from '@/components';
 import { IconButtonPlay } from '@/components/icons';
 
 import styles from './podcastCard.module.scss';
 
-const PodcastCard = ({ posts = [] }) => {
-  interface Post {
+const tp = new Typograf({ locale: ['ru', 'en-US'] });
+
+interface PodcastCardProps {
+  posts: {
     title: string;
     contentSnippet: string;
     isoDate: string;
@@ -20,44 +22,45 @@ const PodcastCard = ({ posts = [] }) => {
     enclosure: {
       url: string;
     };
-  }
+  }[];
+}
 
-  const tp = new typograf({ locale: ['ru', 'en-US'] });
-
-  return (
-    <ul className={styles.wrapper}>
-      {posts.map((post: Post, i) => (
-        <li key={i} className={styles.container}>
-          <div className={styles.wrapper_cover}>
-            <img className={styles.cover} src={post.itunes.image} />
-          </div>
-          <div className={styles.container_info}>
-            <Typography tag='p' variant='text' className={styles.date}>
-              {post.isoDate.slice(0, 10).split('-').reverse().join('.')}
-            </Typography>
-            <Typography tag='h4' variant='title5' className={styles.title}>
-              {post.title.slice(post.title.indexOf('#'))}
-            </Typography>
-            <Typography tag='p' variant='text' className={styles.content}>
-              {tp.execute(post.contentSnippet)}
-            </Typography>
-            <AudioPlayer
-              src={post.enclosure.url}
-              onPlay={(e) => console.log('onPlay')}
-              layout='horizontal-reverse'
-              showJumpControls={false}
-              customVolumeControls={[]}
-              customAdditionalControls={[]}
-              showDownloadProgress={false}
-              customIcons={{
-                play: <IconButtonPlay />,
-              }}
-            />
-          </div>
-        </li>
-      ))}
-    </ul>
-  );
-};
+const PodcastCard: FC<PodcastCardProps> = ({ posts }) => (
+  <ul className={styles.wrapper}>
+    {posts.map((post, i) => (
+      <li key={i} className={styles.container}>
+        <div className={styles.wrapper_cover}>
+          <img
+            alt={post.title.slice(post.title.indexOf('#'))}
+            className={styles.cover}
+            src={post.itunes.image}
+          />
+        </div>
+        <div className={styles.container_info}>
+          <Typography className={styles.date} tag='p' variant='text'>
+            {post.isoDate.slice(0, 10).split('-').reverse().join('.')}
+          </Typography>
+          <Typography className={styles.title} tag='h4' variant='title5'>
+            {post.title.slice(post.title.indexOf('#'))}
+          </Typography>
+          <Typography className={styles.content} tag='p' variant='text'>
+            {tp.execute(post.contentSnippet)}
+          </Typography>
+          <AudioPlayer
+            customAdditionalControls={[]}
+            customVolumeControls={[]}
+            layout='horizontal-reverse'
+            showDownloadProgress={false}
+            showJumpControls={false}
+            src={post.enclosure.url}
+            customIcons={{
+              play: <IconButtonPlay />,
+            }}
+          />
+        </div>
+      </li>
+    ))}
+  </ul>
+);
 
 export default PodcastCard;
