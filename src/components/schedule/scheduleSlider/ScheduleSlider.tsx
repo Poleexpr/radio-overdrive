1f30ed8188b9120cdbd21f2ab5c4489d4be31e90
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-03-05 21:41:52 +0300

    refactor: replace styles

--- src/components/schedule/scheduleSlider/ScheduleSlider.tsx
+++ src/components/schedule/scheduleSlider/ScheduleSlider.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import './scheduleCard.scss';
-
 import { useEffect, useRef } from 'react';
 import { register } from 'swiper/element/bundle';
 
@@ -10,6 +8,7 @@ import { IconArrowNext, IconArrowPrev } from '@/components/icons';
 import ScheduleCard from '../scheduleCard/ScheduleCard';
 
 const Schedule = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const swiperRef = useRef<any>(null!);
 
   useEffect(() => {
@@ -17,14 +16,14 @@ const Schedule = () => {
 
     const params = {
       // slidesPerView: 1,
-      grabCursor: true,
       centeredSlides: true,
-
+      grabCursor: true,
+      loop: true,
       navigation: {
         nextEl: '.custom-next',
         prevEl: '.custom-prev',
       },
-      loop: true,
+
       breakpoints: {
         210: {
           slidesPerView: 1,
@@ -37,6 +36,7 @@ const Schedule = () => {
 
     Object.assign(swiperRef.current, params);
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     swiperRef.current?.initialize();
 
     /* swiperElRef.current?.addEventListener('swiperprogress', (e: any) => {
