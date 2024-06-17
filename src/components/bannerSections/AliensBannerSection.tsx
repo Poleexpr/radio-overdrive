8aad5f3e20ade7f2434ca52b5030610a6f00570a
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-06-17 11:27:27 +0300

    feat: add links to new pages

--- src/components/bannerSections/AliensBannerSection.tsx
+++ src/components/bannerSections/AliensBannerSection.tsx
@@ -1,42 +1,24 @@
 'use client';
 
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-
-import { Podcast, Typography, ButtonRectangle, ErrorText } from '@/components';
-import { useTogglePodcast } from '@/utils';
+import { Typography, LinkRectangle } from '@/components';
 
 import styles from './bannerSections.module.scss';
 
-const AliensBannerSection = () => {
-  const [showPodcast, togglePodcast] = useTogglePodcast(false);
-
-  return (
-    <section className={styles.aliens2}>
-      <div className={styles.container}>
-        <Typography tag='h2' variant='title2'>
-          пришельцы
-        </Typography>
-        <Typography className={styles.description} tag='p' variant='text2'>
-          {' '}
-          Прямые эфиры с музыкантами, писателями, философами и другими гостями не от мира сего.
-        </Typography>
-        <ButtonRectangle className={styles.button} onClick={() => togglePodcast()}>
-          {showPodcast ? 'скрыть' : 'слушать онлайн'}
-        </ButtonRectangle>
-      </div>
-      {showPodcast && (
-        <ErrorBoundary fallback={<ErrorText podcastLink='https://radiooverdrive.mave.digital' />}>
-          <Suspense fallback={<p> Loading...</p>}>
-            <Podcast.Collection
-              feedUrl='./prishelcy'
-              podcastLink='https://overdrive-prishelcy.mave.digital'
-            />
-          </Suspense>
-        </ErrorBoundary>
-      )}
-    </section>
-  );
-};
+const AliensBannerSection = () => (
+  <section className={styles.aliens2}>
+    <div className={styles.container}>
+      <Typography tag='h2' variant='title2'>
+        пришельцы
+      </Typography>
+      <Typography className={styles.description} tag='p' variant='text2'>
+        {' '}
+        Прямые эфиры с музыкантами, писателями, философами и другими гостями не от мира сего.
+      </Typography>
+      <LinkRectangle className={styles.link} href='./aliens'>
+        слушать
+      </LinkRectangle>
+    </div>
+  </section>
+);
 
 export default AliensBannerSection;
--- src/components/bannerSections/OcapellaBannerSection.tsx
+++ src/components/bannerSections/OcapellaBannerSection.tsx
@@ -1,42 +1,24 @@
 'use client';
 
-import { Suspense } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-
-import { Podcast, Typography, ButtonRectangle, ErrorText } from '@/components';
-import { useTogglePodcast } from '@/utils';
+import { Typography, LinkRectangle } from '@/components';
 
 import styles from './bannerSections.module.scss';
 
-const OcapellaBannerSection = () => {
-  const [showPodcast, togglePodcast] = useTogglePodcast(false);
-
-  return (
-    <section className={styles.ocapella}>
-      <div className={styles.container}>
-        <Typography tag='h2' variant='title2'>
-          о’капелла
-        </Typography>
-        <Typography className={styles.description} tag='p' variant='text2'>
-          {' '}
-          Подкаст о музыке как феномене и чуде.
-        </Typography>
-        <ButtonRectangle className={styles.button} onClick={() => togglePodcast()}>
-          {showPodcast ? 'скрыть' : 'слушать онлайн'}
-        </ButtonRectangle>
-      </div>
-      {showPodcast && (
-        <ErrorBoundary fallback={<ErrorText podcastLink='https://radiooverdrive.mave.digital' />}>
-          <Suspense fallback={<p> Loading...</p>}>
-            <Podcast.Collection
-              feedUrl='./ocappella'
-              podcastLink='https://radiooverdrive.mave.digital'
-            />
-          </Suspense>
-        </ErrorBoundary>
-      )}
-    </section>
-  );
-};
+const OcapellaBannerSection = () => (
+  <section className={styles.ocapella}>
+    <div className={styles.container}>
+      <Typography tag='h2' variant='title2'>
+        о’капелла
+      </Typography>
+      <Typography className={styles.description} tag='p' variant='text2'>
+        {' '}
+        Подкаст о музыке как феномене и чуде.
+      </Typography>
+      <LinkRectangle className={styles.link} href='./ocapella'>
+        слушать
+      </LinkRectangle>
+    </div>
+  </section>
+);
 
 export default OcapellaBannerSection;
