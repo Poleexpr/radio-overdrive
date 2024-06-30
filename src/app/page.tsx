2c61713189dbbab835f75577e34ac9be0d52420f
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-06-30 17:51:48 +0300

    refactor: add video to welcome block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ const Home = () => (
           muted
           className={styles.video}
           poster='./videos/wellcomeBackPoster.jpg'
-          src='#'
+          src='./videos/wellcomeBack.mp4'
         />
         <div className={styles.container}>
           <div className={styles.title_wrapper}>
