8d9c91d5aabd657334258ddbe4f67e623bb099ff
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-08-07 00:34:40 +0300

    refactor: specify imports

--- src/utils/data/index.ts
+++ src/utils/data/index.ts
@@ -1,6 +1,6 @@
 export * from './navItemsInfo';
 export * from './ocapellaInfo';
 export * from './aliensInfo';
-export * from './presentersConfig';
-export * from './scheduleConfig';
-export * from './radioConfig';
+export { presentersConfig } from './presentersConfig';
+export { scheduleConfig } from './scheduleConfig';
+export { radioConfig } from './radioConfig';
