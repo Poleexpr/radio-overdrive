c8d18ac9c41c9dfcf0baf598bed694f5ad5057a2
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-08-10 19:05:05 +0300

    refactor: change assignment type

--- src/components/videos/videoWelcome/VideoWelcome.tsx
+++ src/components/videos/videoWelcome/VideoWelcome.tsx
@@ -7,15 +7,16 @@ import { Loader } from '@/components';
 import styles from './videoWelcome.module.scss';
 
 export const VideoWelcome = () => {
-  const [isCanPlay, setIsCanPlay] = useState(false);
+  // eslint-disable-next-line prefer-const
+  let [isCanPlay, setIsCanPlay] = useState(false);
 
   const handleIsCanPlay = () => {
-    setIsCanPlay(!isCanPlay);
+    setIsCanPlay((isCanPlay = true));
   };
 
   if (!isCanPlay) {
     setTimeout(() => {
-      setIsCanPlay(!isCanPlay);
+      setIsCanPlay((isCanPlay = true));
     }, 1000);
   }
 
