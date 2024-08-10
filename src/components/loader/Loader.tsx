a8ec823f8a0621627b38096fb18cacea455859f6
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-08-10 16:17:08 +0300

    feat: change loader style

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -4,6 +4,8 @@ import styles from './loader.module.scss';
 
 export const Loader = () => (
   <div className={styles.loaderWrapper}>
+    <span className={styles.circleOuter} />
+    <span className={styles.circleInner} />
     <div className={styles.loader}>
       <IconLogoOverdrive />
     </div>
