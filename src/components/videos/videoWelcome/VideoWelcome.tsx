9057002c52da4db4a048f960a697e1af72f997d9
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-08-15 01:56:29 +0300

    refactor: remove the loader

--- src/components/videos/videoWelcome/VideoWelcome.tsx
+++ src/components/videos/videoWelcome/VideoWelcome.tsx
@@ -1,38 +1,13 @@
-'use client';
-
-import { useState } from 'react';
-
-import { Loader } from '@/components';
-
 import styles from './videoWelcome.module.scss';
 
-export const VideoWelcome = () => {
-  // eslint-disable-next-line prefer-const
-  let [isCanPlay, setIsCanPlay] = useState(false);
-
-  const handleIsCanPlay = () => {
-    setIsCanPlay((isCanPlay = true));
-  };
-
-  if (!isCanPlay) {
-    setTimeout(() => {
-      setIsCanPlay((isCanPlay = true));
-    }, 3000);
-  }
-
-  return (
-    <>
-      {!isCanPlay && <Loader />}
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className={styles.video}
-        poster='./videos/wellcomeBackPoster.webp'
-        src='./videos/wellcomeBack.mp4'
-        onCanPlay={handleIsCanPlay}
-      />
-    </>
-  );
-};
+export const VideoWelcome = () => (
+  <video
+    autoPlay
+    loop
+    muted
+    playsInline
+    className={styles.video}
+    poster='./videos/wellcomeBackPoster.webp'
+    src='./videos/wellcomeBack.mp4'
+  />
+);
